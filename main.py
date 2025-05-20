39a0a2ac7283b26990c93ed6913d7f3ac9579286
phat114 <81601273+phat114@users.noreply.github.com>
2025-05-20 18:25:43 +0700

    u[date

--- main.py
+++ main.py
@@ -115,3 +115,9 @@ async def main():
             print(f"❌ Lỗi khởi động crawl: {e}")
             logging.error(f"❌ Lỗi khởi động crawl: {e}", exc_info=True)
 
+if __name__ == '__main__':
+    # if platform.system() == 'Windows':
+        # This mitigates a warning raised by curl-cffi. If you do not need to use curl-impersonate, you may remove this.
+        # asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+    asyncio.run(main())
